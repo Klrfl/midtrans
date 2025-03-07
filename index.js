7e7c836eab195057e25defc2b50756cf2e36b1dd
Efraim Munthe <efrayanglain@gmail.com>
2025-03-07 11:30:21 +0700

    feat: provide default export for backwards compatibility

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+import { CoreApi } from './lib/coreApi.js';
+import { Iris } from './lib/iris.js';
+import { MidtransError } from './lib/midtransError.js';
+import { Snap } from './lib/snap.js';
+import { SnapBi } from './lib/snapBi/snapBi.js';
+import { SnapBiConfig } from './lib/snapBi/snapBiConfig.js';
+
 export { Snap } from './lib/snap.js';
 export { CoreApi } from './lib/coreApi.js';
 export { Iris } from './lib/iris.js';
 export { MidtransError } from './lib/midtransError.js';
 export { SnapBiConfig } from './lib/snapBi/snapBiConfig.js';
 export { SnapBi } from './lib/snapBi/snapBi.js';
+
+export default {
+  Snap,
+  CoreApi,
+  Iris,
+  MidtransError,
+  SnapBiConfig,
+  SnapBi,
+};
